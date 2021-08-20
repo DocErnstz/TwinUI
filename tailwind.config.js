efeed5556f26b8c7a744549f7b65bcc0b8fdf954
DocErnstz <ernesto800@outlook.com>
2021-08-20 16:22:02 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      Noto: ["Noto Sans, sans-serif"],
+    },
+    container: {
+      center: true,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

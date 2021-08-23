81112935f4414cb5f50276c3ad31ce3491ef7cf9
Suyai <ernesto800@outlook.com>
2021-08-23 15:45:48 -0300

    static sidebar left set

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: theme => ({
+        "screen/2": "50vh",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+        "screen-almost": "88vh",
+        "screen-80": "80vh",
+      }),
+      gridTemplateColumns: {
+       'profileAd': '48px 1fr 1fr',
+      }
+
+    },
     fontFamily: {
       Noto: ["Noto Sans, sans-serif"],
     },
